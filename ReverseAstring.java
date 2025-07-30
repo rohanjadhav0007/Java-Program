653bfc54d7f5744dbe1bde8bb3daabd2d9905256
Rohan Jadhav <rohanjadhav23112003@gmail.com>
2025-07-30 15:25:05 +0530

    initial commit

---
+++ ReverseAstring.java
@@ -0,0 +1,7 @@
+public class ReverseAstring { 
+    public static void main(String[] args) {
+        String str="Rohan,Jadhav";
+        StringBuilder str1= new StringBuilder(str).reverse();
+        System.out.println(str1);
+    }   
+}
